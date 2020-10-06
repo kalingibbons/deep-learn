18465521c5aa709f95475ca693f832f18d5ee61a
Kalin Gibbons <kalingibbons@gmail.com>
2020-10-06 14:32:49 -0600

    Functional version of hw01 working.

--- spiral_hw_01/past/gibbons_hw_01_3.py
+++ spiral_hw_01/past/gibbons_hw_01_3.py
@@ -14,21 +14,11 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-try:
-    from spiral_training_data import generate_spiral_data
-except:
-    import Spiral_training_data
-
-start = time.time()
-
 
 # %%
 # initialize parameters randomly
 try:
-    (n_classes, n_dims, n_points, n_hidden) = (K, D, N, h)
-    (data_mat, true_labels) = (Xt, y)
-
-except NameError:
+    from spiral_training_data import generate_spiral_data
     n_classes = 3
     n_dims = 2
     n_points = 100
@@ -37,33 +27,22 @@ except NameError:
                                                  n_dims=n_dims,
                                                  n_points=n_points)
 
-
-def one_hot(array):
-    y = array.copy()
-    oh = np.zeros((y.size, y.max() + 1))
-    oh[np.arange(y.size), y] = 1
-    return oh
-
-
-true_labels = one_hot(true_labels).T
-# sizes = [n_dims, n_hidden, n_classes]
-# y = [y for y in sizes[1:]]
-# x = [x for x in sizes[:-1]]
-
-weight_2 = 0.01 * np.random.randn(n_hidden, n_dims)
-bias_2 = np.zeros((n_hidden, 1))
-
-weight_3 = 0.01 * np.random.randn(n_classes, n_hidden)
-bias_3 = np.zeros((n_classes, 1))
-
-# some hyperparameters
-learn_rate = 1e-0  # learning rate / step size. Eta in formulas
-reg_strength = 0*1e-3  # regularization strength. Lambda in formulas
+except ImportError:
+    (n_classes, n_dims, n_points, n_hidden) = (K, D, N, h)
+    (data_mat, true_labels) = (Xt, y)
 
 
 # %%
-def relu(vals):
-    return np.maximum(0, vals)
+def encode(array):
+    y = array.copy()
+    oh = np.zeros((y.size, y.max() + 1))
+    oh[np.arange(y.size), y] = 1
+    return oh.T
+
+
+def decode(one_hot, axis=0):
+    y = one_hot.copy()
+    return y.argmax(axis=axis)
 
 
 def sigmoid(z):
@@ -82,72 +61,147 @@ def update_parameter(parameter, parameter_partial):
     return parameter - learn_rate * parameter_partial
 
 
-def add_regularization(weight, weight_partial):
-    return weight_partial + (reg_strength / n_observation) * weight
-    # return weight_partial + reg_strength * weight
+def regularize_cost(strength, n_obs, weights):
+    ss = [np.sum(w ** 2) for w in weights]
+    return strength / 2 / n_obs * np.sum(ss)
 
 
-def ss(arr):
-    return np.sum(arr ** 2)
+def quadratic_cost(true, predictions, n_observation):
+    return 0.5 * ((true - predictions) ** 2).sum() / n_observation
+
+
+def calculate_cost(true, pred, n_obs, strength, weights):
+    cost = (quadratic_cost(true, pred, n_obs)
+            + regularize_cost(strength, n_obs, weights))
+    return cost
+
+
+def feed_forward(inp, weight, bias, activation=sigmoid):
+    z = weighted_input(weight, inp, bias)
+    act = activation(z)
+    return z, act
+
+
+def calc_err(true, pred, final_weight, n_obs, activation_prime=sigmoid_prime):
+    return (pred - true) * activation_prime(final_weight) / n_obs
+
+
+def backprop_err(later_err,
+                 later_weight,
+                 curr_weighted_input,
+                 activation_prime=sigmoid_prime):
+    return (later_weight.T @ later_err) * activation_prime(curr_weighted_input)
+
+
+def backprop_bias(current_error, axis=1):
+    return current_error.sum(axis=axis, keepdims=True)
+
+
+def backprop_weight(err, inp, strength, n_obs, weight):
+    return err @ inp.T + (strength / n_obs) * weight
+
+
+def regularize_weight_prime(weight, weight_partial):
+    return weight_partial + (reg_strength / n_obs) * weight
+
+
+# %%
+start = time.time()
+true_labels = encode(true_labels)
+
+weight_2 = 0.01 * np.random.randn(n_hidden, n_dims)
+bias_2 = np.zeros((n_hidden, 1))
+
+weight_3 = 0.01 * np.random.randn(n_classes, n_hidden)
+bias_3 = np.zeros((n_classes, 1))
+
+# some hyperparameters
+learn_rate = 1e-0  # learning rate / step size. Eta in formulas
+reg_strength = 0e-3  # regularization strength. Lambda in formulas
 
 
 # %%
 # gradient descent loop
 X = np.transpose(data_mat)
-n_observation = X.shape[1]
+n_obs = X.shape[1]
 n_epochs = int(1e5)
-cost_scaler = 0.5 * (reg_strength / n_observation)
-# cost_scaler = 0.5 * (reg_strength / 1)
+
+weights = [weight_2, weight_3]
+biases = [bias_2, bias_3]
 
 for epoch in range(n_epochs):
 
+    # --- Feed Forward ---
     # Compute weighted inputs and activations
-    weight_inp_2 = weighted_input(weight_2, X, bias_2)
-    activation_2 = sigmoid(weight_inp_2)
+    weight_inp_2, active_2 = feed_forward(X, weight_2, bias_2)
+    weight_inp_3, active_3 = feed_forward(active_2, weight_3, bias_3)
 
-    weight_inp_3 = weighted_input(weight_3, activation_2, bias_3)
-    activation_3 = sigmoid(weight_inp_3)
-
-    # Compute the loss
-    cost_y = 0.5 * ((true_labels - activation_3) ** 2)
-    avg_cost_y = cost_y.sum() / n_observation
-    cost_regularization = cost_scaler * np.sum((ss(weight_2), ss(weight_3)))
-    cost = avg_cost_y + cost_regularization
+    # --- Training Metrics ---
+    # Compute and print the regularized quadratic cost
+    cost = calculate_cost(
+        true=true_labels,
+        pred=active_3,
+        n_obs=n_obs,
+        strength=reg_strength,
+        weights=(weight_2, weight_3)
+    )
     if epoch % 1000 == 0:   # % -  modulo
         print(f'Epoch {epoch:04d}: loss {cost:.5f}')
 
-    # Compute the error term
-    delta_3 = (activation_3 - true_labels) * sigmoid_prime(weight_inp_3)
-    delta_3 /= n_observation
+    # ---- Backpropagation ----
+    # Initialize backpropagation using the final layer
+    activations = [X, active_2]
+    weights = [weight_2, weight_3]
+    biases = [bias_2, bias_3]
+    weighted_inputs = [weight_inp_2, weight_inp_3]
+    delta = calc_err(true_labels, active_3, weight_inp_3, n_obs)
+    for idx in reversed(range(len(weights))):
+        a = activations[idx]
+        w = weights[idx]
+        b = biases[idx]
+        z = weighted_inputs[idx - 1]
+        weight_pprime = backprop_weight(delta, a, reg_strength, n_obs, w)
+        bias_pprime = backprop_bias(delta, axis=1)
+        weights[idx] = update_parameter(w, weight_pprime)
+        biases[idx] = update_parameter(b, bias_pprime)
+        if idx > 0:
+            delta = backprop_err(delta, w, z)
 
-    # Use backprop to get the gradient of the parameters
-    # First backprop into parameters W3 and b3
-    weight_3_partial = delta_3 @ activation_2.T
-    weight_3_partial = add_regularization(weight_3, weight_3_partial)
-    bias_3_partial = delta_3.sum(axis=1, keepdims=True)
+    weight_2, weight_3 = weights
+    bias_2, bias_3 = biases
 
-    # Next backprop into hidden layer
-    delta_2 = (weight_3.T @ delta_3) * sigmoid_prime(weight_inp_2)
+    # delta = calc_err(true_labels, active_3, weight_inp_3, n_obs)
+    # weight_3_partial = backprop_weight(err=delta,
+    #                                    inp=active_2,
+    #                                    strength=reg_strength,
+    #                                    n_obs=n_obs,
+    #                                    weight=weight_3)
 
-    # Finally into W,b
-    weight_2_partial = delta_2 @ X.T
-    weight_2_partial = add_regularization(weight_2, weight_2_partial)
-    bias_2_partial = delta_2.sum(axis=1, keepdims=True)
+    # bias_3_partial = backprop_bias(delta, axis=1)
 
-    # Update weights and biases
-    weight_2 = update_parameter(weight_2, weight_2_partial)
-    bias_2 = update_parameter(bias_2, bias_2_partial)
+    # # Next backpropagate into previous (hidden layer)
+    # delta = backprop_err(delta, weight_3, weight_inp_2)
+    # weight_2_partial = backprop_weight(err=delta,
+    #                                    inp=X,
+    #                                    strength=reg_strength,
+    #                                    n_obs=n_obs,
+    #                                    weight=weight_2)
 
-    weight_3 = update_parameter(weight_3, weight_3_partial)
-    bias_3 = update_parameter(bias_3, bias_3_partial)
+    # bias_2_partial = backprop_bias(delta, axis=1)
+
+    # # --- Parameter Updates ---
+    # weight_2 = update_parameter(weight_2, weight_2_partial)
+    # bias_2 = update_parameter(bias_2, bias_2_partial)
+
+    # weight_3 = update_parameter(weight_3, weight_3_partial)
+    # bias_3 = update_parameter(bias_3, bias_3_partial)
 
 # evaluate training set accuracy
-activation_2 = sigmoid(weighted_input(weight_2, X, bias_2))
-activation_3 = weighted_input(weight_3, activation_2, bias_3)
-predicted_class = activation_3.argmax(axis=0)
-print(f'training accuracy: {np.mean(predicted_class == true_labels):.4f}')
-misses = np.argwhere(predicted_class != true_labels)
-#
+predicted_labels = decode(active_3)
+true_labels = decode(true_labels)
+print(f'training accuracy: {np.mean(predicted_labels == true_labels):.4f}')
+misses = np.argwhere(predicted_labels != true_labels)
+
 # plot the resulting classifier
 g = 0.02
 x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
@@ -169,24 +223,10 @@ Z = np.argmax(z3_grid, axis=0)
 Z = Z.reshape(xx.shape)
 fig = plt.figure()
 plt.contourf(xx, yy, Z, cmap=plt.cm.winter, alpha=0.3)
-plt.scatter(X[0, :], X[1, :], c=true_labels.argmax(axis=0), s=40, cmap=plt.cm.winter)
+plt.scatter(X[0, :], X[1, :], c=true_labels, s=40, cmap=plt.cm.winter)
 plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
 fig.savefig('spiral_net.png')
 
 end = time.time()
 print(f'Time needed to run program: {end - start:.4f} seconds')
-
-print(activation_3.shape)
-print("")
-print(activation_3)
-print("")
-print(true_labels.shape)
-print("")
-print(true_labels)
-print("")
-print("true_labels-activation_3")
-print("")
-print(true_labels-activation_3)
-print("")
-print("Incorrect Use of broadcasting!!!")

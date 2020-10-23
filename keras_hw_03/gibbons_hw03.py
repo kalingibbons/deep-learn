b129f1327640fdbb5c81e01159cba68f7c80a804
Kalin Gibbons <kalingibbons@gmail.com>
2020-10-22 18:17:19 -0600

    Got the code cleaned up

--- keras_hw_03/gibbons_hw03.py
+++ keras_hw_03/gibbons_hw03.py
@@ -1,101 +1,92 @@
 # %% [markdown]
-#  # Create a Headline
-# Create a new cell.
-# Put the cursor in the new cell.
-# Press Esc key.
-# Press m key.
-# Write your comment.
-# Press shift+Enter keys.
+# # Homework 03
 #
+# Kalin Gibbons
+
 
 # %% [markdown]
 # # Load MNIST Data
 #
 
 # %%
+from matplotlib.pyplot import imshow
 import numpy as np
 import matplotlib.pyplot as plt
+
 from keras import layers, models
 from keras.datasets import mnist
 from keras.utils import to_categorical
+from keras import regularizers, initializers
+
+
+# %% [markdown]
+# #  Load the data and plot the digits
 
 
 # %%
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-
-# %% [markdown]
-# # MNIST Data Format
-
-# %%
-print(train_images.shape)
-print(len(train_images))
-print(train_images.ndim)
-
-
-# %%
-# test_images[0]   # 28x28 digits in grey level 0-255
-
-
-# %%
-train_labels
-
-
-# %%
-print('test_images.shape:{}'.format(test_images.shape))
-print('test_labels.ndim:{}'.format(test_images.ndim))
-print('len(test_images):{}'.format(len(test_images)))
-
-
-# %%
-# print('test_image_sample  {}'.format(test_images[0]))
-test_labels
-
-# %% [markdown]
-# #  Plot the Digits
-
-# %%
 digit = train_images[1]
+im_shape = digit.shape
+n_train = train_images.shape[0]
+n_test = test_images.shape[0]
 plt.imshow(digit, cmap=plt.cm.binary)
 plt.show()
 print(train_labels[0:10])
 
+
 # %% [markdown]
-# # Prepare the Images
+# # Encode the image data
+#
+# Need to unravel the 2D image arrays into a 1D array.
+
 
 # %%
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.reshape((n_train, np.prod(im_shape)))
 train_images = train_images.astype('float32') / 255
-test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.reshape((n_test, np.prod(im_shape)))
 test_images = test_images.astype('float32') / 255
 print('train_images.shape: {}'.format(train_images.shape))
 print('train_images.ndim: {}'.format(train_images.ndim))
 
+
 # %% [markdown]
-# # Prepare the Labels
+# # Encode the Labels
+#
+# Need to convert the labels to one-hot encoding.
+
 
 # %%
-train_labels = to_categorical(train_labels)  # Convert labels to 1-hot
-test_labels = to_categorical(test_labels)    # Convert labels to 1-hot
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
 print('train_labels.shape: {}'.format(train_labels.shape))
 print('train_labels.ndim {}'.format(train_labels.ndim))
 print(train_labels[0])
+
 
 # %% [markdown]
 # # The Network Architecture
 
 # %%
 network = models.Sequential()  # Specify layers in their sequential order
-# inputs are vectors in R^28*28 = R^784
-# Dense = Fully Connected.
-# Hidden layer has 512 neurons with ReLU activations.
-network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
-# Ouput layer uses softmax with 10 ouput neurons
-# Assume there are 512 neurons going into the output layer
-network.add(layers.Dense(10, activation='softmax'))  # sigmoid relu
+reg_strength = 0.001
+hidden_layers = (
+    layers.Dense(units=512,
+                 activation='relu',
+                 kernel_initializer=initializers.glorot_normal(),
+                 kernel_regularizer=regularizers.l2(reg_strength),
+                 input_shape=(np.prod(im_shape), )),
+)
+final_layer = layers.Dense(units=10, activation='softmax')
+
+# Assemble the network architecture
+for h_layer in hidden_layers:
+    network.add(h_layer)
+network.add(final_layer)
+
 
 # %% [markdown]
 # # Compile the Network
+
 
 # %%
 network.compile(
@@ -106,15 +97,19 @@ network.compile(
 # Options for  optimizer = 'adam', 'sgd'
 # Other Options loss = 'mean_squared_error' or 'mse'
 
+
 # %% [markdown]
 # # Train the Network
+
 
 # %%
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
 #  "fit" refers to fitting the network weights to the data
 
+
 # %% [markdown]
 # # Check Accuracy on Test Data
+
 
 # %%
 network.evaluate(np.array(test_images),
